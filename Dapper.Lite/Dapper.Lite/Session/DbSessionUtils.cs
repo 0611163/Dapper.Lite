e95495ccb9246e21a0024651ac6e25618c136e89
suxiang <651029594@qq.com>
2023-10-26 13:28:45 +0800

    修复bug：标注自增字段但不是主键 插入报错

--- Dapper.Lite/Dapper.Lite/Session/DbSessionUtils.cs
+++ Dapper.Lite/Dapper.Lite/Session/DbSessionUtils.cs
@@ -77,19 +77,19 @@ namespace Dapper.Lite
                     if (propertyInfo.GetCustomAttribute<KeyAttribute>() != null)
                     {
                         propertyInfoEx.IsDBKey = true;
+                    }
 
-                        AutoIncrementAttribute modelAutoIncrementAttribute = modelType.GetCustomAttribute<AutoIncrementAttribute>(false);
-                        if (modelAutoIncrementAttribute != null)
+                    AutoIncrementAttribute modelAutoIncrementAttribute = modelType.GetCustomAttribute<AutoIncrementAttribute>(false);
+                    if (modelAutoIncrementAttribute != null)
+                    {
+                        propertyInfoEx.IsAutoIncrement = modelAutoIncrementAttribute.Value;
+                    }
+                    else
+                    {
+                        AutoIncrementAttribute propertyAutoIncrementAttribute = propertyInfo.GetCustomAttribute<AutoIncrementAttribute>(false);
+                        if (propertyAutoIncrementAttribute != null)
                         {
-                            propertyInfoEx.IsAutoIncrement = modelAutoIncrementAttribute.Value;
-                        }
-                        else
-                        {
-                            AutoIncrementAttribute propertyAutoIncrementAttribute = propertyInfo.GetCustomAttribute<AutoIncrementAttribute>(false);
-                            if (propertyAutoIncrementAttribute != null)
-                            {
-                                propertyInfoEx.IsAutoIncrement = propertyAutoIncrementAttribute.Value;
-                            }
+                            propertyInfoEx.IsAutoIncrement = propertyAutoIncrementAttribute.Value;
                         }
                     }
 
